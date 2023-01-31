b20b3a49d3f85c148ed5b724716d06e52cee5b99
ASyrotenko <andreysyrotenko@gmail.com>
2023-01-31 15:12:49 +0200

    add tablet and mobile styles

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+// Hide banner
+
 const bannerSection = document.querySelector(".banner");
 const bannerCloseBtn = document.querySelector(".banner__close-btn");
 
@@ -6,3 +8,24 @@ const closeBanner = () => {
 };
 
 bannerCloseBtn.addEventListener("click", closeBanner);
+
+// See more
+
+const moreText = document.querySelector(".brief-disc");
+const readMoreBtn = document.querySelector(".brief__read-more-btn");
+
+const showMoreText = () => {
+  moreText.classList.toggle("text-hidden");
+
+  const initialText = "see more...";
+
+  if (
+    readMoreBtn.textContent.toLowerCase().includes(initialText.toLowerCase())
+  ) {
+    readMoreBtn.textContent = "see less...";
+  } else {
+    readMoreBtn.textContent = initialText;
+  }
+};
+
+readMoreBtn.addEventListener("click", showMoreText);
